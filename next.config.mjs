3ecb9e00f539ee2558a5715cdf053e243cabf10d
Rolino Ongco <ongcorj@gmail.com>
2026-03-04 09:01:05 +0800

    next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        // IMPORTANT: include your backend API prefix here
+        destination: "https://polsight-backend-production.up.railway.app/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
